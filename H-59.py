627912b8b4b761e9512faf5a2580be837f197406
AisyahShintaBalqis <shintabalqis30301@gmail.com>
2022-02-08 18:46:03 +0700

    H59

---
+++ H-59.py
@@ -0,0 +1,13 @@
+def luasSegitiga(alas, tinggi):
+    luas = (alas * tinggi) / 2
+    print ("Luas segitiga: ",luas)
+
+# Pemanggilan fungsi
+luasSegitiga(4, 6)
+
+def persegi(sisi):
+    luas = (sisi* sisi)
+    print ("Luas Persegi: ",luas)
+
+# Pemanggilan fungsi
+persegi(4)
